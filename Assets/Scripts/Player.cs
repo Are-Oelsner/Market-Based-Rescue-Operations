6547baea14734e46e60ccf76030c1868e6df4856
Calvin Bauer <calvinbauer@calvins-mbp.devices.brown.edu>
2022-10-24 12:05:39 -0400

    Added simple Euclidean distance based path planning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,16 +18,44 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-
+        InvokeRepeating("Path_Nav", 2.0f, 1.0f);
     }
+
     // Update is called once per frame
-    void Update()
+    void Path_Nav()
     {
-        
+        Vector3 goal_pos = GameObject.Find("Agent (1)").transform.position; 
+
+        if(Vector3.Distance(goal_pos, transform.position) < 0.5f)
+        {
+            return;
+        }
+
+        Vector3[] transforms = {new Vector3(1,0,0) + transform.position, new Vector3(-1,0,0) + transform.position, new Vector3(0,1,0) + transform.position, new Vector3(0,-1,0) + transform.position};
+        float[] dists = new float[4];
+
+        for(int i = 0; i < 4; i++)
+        {
+            dists[i] = Vector3.Distance(transforms[i], goal_pos);
+        }
+
+        int min_dist = get_min_index(dists);
+        transform.position = transforms[min_dist];
+
     }
 
-    void move_to(float goal_x, float goal_y)
+    int get_min_index(float[] arr)
     {
+        int min_ind = 0;
+        for(int i = 0; i<arr.Length; i++)
+        {
+            if(arr[i] < arr[min_ind])
+            {
+                min_ind = i;
+            }
 
+        }
+        return min_ind;
     }
+
 }
