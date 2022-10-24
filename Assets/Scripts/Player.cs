79501979be7759ccc07732ef3d1c6ab9396e93f2
Are-Oelsner <are_oelsner@brown.edu>
2022-10-24 11:08:57 -0400

    feat: player script in progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float position_x;
+    public float position_y;
+    public int num_gas_masks;
+
     // Start is called before the first frame update
-    void Start()
+    void Start(float start_position_x, float start_position_y, int gas_masks)
     {
-        
+        position_x = start_position_x;
+        position_y = start_position_y;
+        num_gas_masks = gas_masks;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void move_to(float goal_x, float goal_y)
+    {
+
     }
 }
